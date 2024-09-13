d96182cc735aaf89602d3b3ddda8d029b18e7b45
nakyeonko3 <badaclock@gmail.com>
2024-09-13 21:27:04 +0900

    style: style: 글로벌 스타일 세팅, 폰크기레이아웃 세팅 (#6) (#6)
    - GlobalStyles.tsx에 글로벌 스타일을 만듬
    - 현재 phone 레이아웃은 App.tsx에서 appContainerLayoutStyle과
      appPhoneLayoutStyle 두가지를 이용해서 스타일을 만듬.
    - theme.ts에 정의해둔 속성들을 사용할 수 있음.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import GlobalStyles from './styles/GlobalStyles';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
